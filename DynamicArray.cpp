13f2c65937cc53c7dc3658f379504dd3412bd6c4
Mitrofan <118289864+ZloyKorovanovich@users.noreply.github.com>
2023-01-24 20:07:35 +0300

    Add files via upload

--- DynamicArray.cpp
+++ DynamicArray.cpp
@@ -1,47 +1,116 @@
 #include <iostream>
+#include <string>
 
+template<typename T>
 class DynamicArray
 {
-private: 
-	int* array;
-	int maxLength;
-	int length;
-	
 public:
 	
-	DynamicArray(int[], int lemgth);
-	
-	void Add(int element, int scaleIncrese);
-};
-
-
-
-void DynamicArray::Add(int element, int scaleIncrease)
-{
-	if(length == maxLength)
+	DynamicArray(T* element, unsigned size)
 	{
-		maxLength += scaleIncrease;
-		int* array2 = new int[maxLength];
-		for(int i = 0; i < length; i++)
-		{
-			array2[i] = array[i];
-		}
-		delete array;
-		array = array2;
+		_sourceArray = element;
+		_size = size;
+		_capacity = size * 2;
 	}
-	array[length] = element;
-	length++;
-}
-operator [](int index) // Fix
-{
-	if(index >= length)
-		index = length-1;
-	return array[index];
-}
-
-
+	
+	DynamicArray(T* element, unsigned size, unsigned capacity)
+	{
+		_sourceArray = element;
+		_size = size;
+		_capacity = capacity;
+	}
+	
+	DynamicArray()
+	{
+		_capacity = 2;
+		_sourceArray = new T[0];
+	}
+	
+	DynamicArray(unsigned capacity)
+	{
+		_capacity = capacity;
+		_sourceArray = new T[0];
+	}
+	
+	DynamicArray(DynamicArray& rght) = delete;
+	DynamicArray(DynamicArray&& rght) = delete;
+	
+	DynamicArray& operator=(DynamicArray& rght) = delete;
+	
+	~DynamicArray()
+	{
+		delete[] _sourceArray;
+	}
+	
+	void Add(T element)
+	{
+		PrivateAdd(element, 2);
+	}
+	
+	void Add(T element, unsigned capacityMultiplier)
+	{
+		PrivateAdd(element, capacityMultiplier);
+	}
+	
+	T operator[](unsigned index) const
+	{
+		return _sourceArray[index];
+	}
+	
+	unsigned Size() const
+	{
+		return _size;
+	}
+	
+	unsigned Capacity() const
+	{
+		return _capacity;
+	}
+	
+private:
+	
+	unsigned _size;
+	unsigned _capacity;
+	T* _sourceArray;
+	
+	void PrivateAdd(T element, unsigned capacityMultiplier)
+	{
+		if(_size == _capacity)
+		{
+			_capacity *= capacityMultiplier;
+			T* tmp = new T[_capacity];
+			
+			for(int i = 0; i < _size; i++)
+				tmp[i] = _sourceArray[i];
+					
+			delete[] _sourceArray;
+			_sourceArray = tmp;
+		}
+		
+		_sourceArray[_size] = element;
+		_size++;
+	}
+	
+};
 
 int main()
 {
+	DynamicArray<int> myArray();
+
+	
+	int input;
+	
+	while(std::cin>>input)
+	{
+		myArray.Add(int(input));
+	}
+
+	
+	for(int i = 0; i < myArray.Size(); i++)
+	{
+		std::cout << myArray[i];
+		std::cout << std::endl;
+	}
+	
 	return 0;
 }
